0b59e01f29f4e4b2c30b6528cbbb04438cde76ed
Chris Eplett <chris@wouldja.com>
2011-02-27 15:04:59 -0500

    More styling for the character sheet

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,9 @@
 $(function() {
 	console.log('foobar');
 	$('a.power').live('click', function() {
+		var li = $(this).parent();
+		li.siblings().removeClass('selected');
+		li.addClass('selected');
 		$('iframe#power').attr('src', this.href);
 		return false;
 	});
