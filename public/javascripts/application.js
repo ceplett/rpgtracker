83a4e7977d808d9c90c386592553ee343aaf4162
Chris Eplett <chris@wouldja.com>
2011-02-27 11:15:15 -0500

    Display the compendium entry for a power in an iframe

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+	console.log('foobar');
+	$('a.power').live('click', function() {
+		$('iframe#power').attr('src', this.href);
+		return false;
+	});
+})
